75fa33c1a7474fd8e422c89030b9760ae985caed
SRVivek1 <16172793+SRVivek1@users.noreply.github.com>
2024-01-21 00:15:02 +0530

    variables in python

---
+++ 04-variable-declarations/declaration-assignment.py
@@ -0,0 +1,22 @@
+"""
+    This program demonstrates different assignment ways.
+"""
+
+id = 10001
+name, age, loc = 'Rahul', 30, 'India'
+
+print('Id : ', id)
+print(f'Name : {name}')
+print('Age : {} \nLocation : {}'.format(age, loc))
+
+
+#
+# Output
+# ----------------
+# /usr/bin/python3 /home/viveksingh/wrkspace/python-basics-examples/04-variable-declarations/declaration-assignment.py
+# Id :  10001
+# Name : Rahul
+# Age : 30
+# Location : India
+#
+#
---
+++ 04-variable-declarations/loosely-typed-declaration.py
@@ -0,0 +1,42 @@
+"""
+    Unlike java and other typed programming languages in python a variable cam hold any type of value.
+
+"""
+
+
+# int
+x = 1
+print(f'type(x) : {type(x)}')
+
+# str
+x = 'hello'
+print(f'type(x) : {type(x)}')
+
+# bool
+x = False
+print(f'type(x) : {type(x)}')
+
+# dict
+x = {1: 'one', 2: 'two'}
+print(f'type(x) : {type(x)}')
+
+# list
+x = [1, 2, 3]
+print(f'type(x) : {type(x)}')
+
+# none
+x = None
+print(f'type(x) : {type(x)}')
+
+
+#
+# Output
+# ----------------
+# /usr/bin/python3 /home/viveksingh/wrkspace/python-basics-examples/04-variable-declarations/loosely-typed-declaration.py
+# type(x) : <class 'int'>
+# type(x) : <class 'str'>
+# type(x) : <class 'bool'>
+# type(x) : <class 'dict'>
+# type(x) : <class 'list'>
+# type(x) : <class 'NoneType'>
+#
