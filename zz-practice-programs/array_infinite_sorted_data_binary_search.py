298d049d74d7f2286a010544540acc9aaa517cac
SRVivek1 <16172793+SRVivek1@users.noreply.github.com>
2024-01-21 14:55:22 +0530

    search in sorted infinite array

---
+++ zz-practice-programs/array_infinite_sorted_data_binary_search.py
@@ -0,0 +1,76 @@
+"""
+ This program demonstrates how to search in an infinite array.
+"""
+
+
+def binary_search(a, l, r, k):
+    """
+    Find the key in array using search technique.
+
+    :param a:
+    :param l:
+    :param r:
+    :param k:
+    :return:
+    """
+    while r >= l:
+
+        mid = l + (r - l) // 2
+
+        if a[mid] == k:
+            return mid
+
+        if a[mid] > k:
+            return binary_search(a, l, mid - 1, k)
+        else:
+            return binary_search(a, mid + 1, r, k)
+
+    return -1
+
+
+def search(data, key):
+    """
+    Search for key in an infinite array.
+    :param arr:
+    :param key:
+    :return:
+    """
+
+    '''
+        find sub array where data exists
+        we have left so we'll find right index 
+        which is when val becomes greater than key.
+    '''
+    left, right, val = 0, 1, data[0]
+    while val < key:
+        left = right
+        right = right * 2
+        val = data[right]
+
+    '''find using binary search'''
+    return binary_search(data, left, right, key)
+
+
+# Driver program
+if __name__ == '__main__':
+    arr = [3, 5, 7, 9, 10, 90, 100, 130, 140, 160, 170]
+
+    key = 10
+    index = search(arr, key)
+    print(f"{key} index: {index}")
+
+    key = 2
+    index = search(arr, key)
+    print(f"{key} index: {index}")
+
+    key = 105
+    index = search(arr, key)
+    print(f"{key} index: {index}")
+
+    key = 0
+    index = search(arr, key)
+    print(f"{key} index: {index}")
+
+    key = 130
+    index = search(arr, key)
+    print(f"{key} index: {index}")
