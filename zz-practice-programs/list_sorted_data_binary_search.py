460dcf251e5a971f34a79c60e0274116e91513da
SRVivek1 <16172793+SRVivek1@users.noreply.github.com>
2024-01-21 14:17:02 +0530

    binary search in list

---
+++ zz-practice-programs/list_sorted_data_binary_search.py
@@ -0,0 +1,55 @@
+"""
+    This program demonstrates the use of binary search on list data.
+"""
+
+
+def binary_search(data, left, right, k):
+    while right >= left:
+
+        mid = left + (right - left)//2
+        if data[mid] == k:
+            return mid
+
+        if data[mid] > k:
+            '''search left sub array'''
+            return binary_search(data, left, mid - 1, k)
+
+        else:
+            '''Search right sub array'''
+            return binary_search(data, mid + 1, right, k)
+
+    '''Return -1 as index if data not found'''
+    return -1
+
+
+def search(data, skey):
+    low, high = 0, len(data) - 1
+    return binary_search(data, low, high, skey)
+
+
+# Driver program
+if __name__ == '__main__':
+    arr = [3, 5, 7, 9, 10, 90, 100, 130, 140, 160, 170]
+
+    key = 10
+    index = search(arr, key)
+    print(f"{key} index: {index}")
+
+    key = 170
+    index = search(arr, key)
+    print(f"{key} index: {index}")
+
+    key = 105
+    index = search(arr, key)
+    print(f"{key} index: {index}")
+
+    key = 0
+    index = search(arr, key)
+    print(f"{key} index: {index}")
+
+    key = 130
+    index = search(arr, key)
+    print(f"{key} index: {index}")
+#
+#
+#
